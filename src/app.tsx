972cc2319bbd9c0fa99934e65b116983c818ab36
Pavwos <rspavanelli@gmail.com>
2024-07-09 19:01:10 -0300

    second-commit

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+export function App() {
+  return (
+    <div className="max-w-3xl px-6">
+      <p>Convide seus amigos e planeje sua próxima viagem!</p>
+      <div>
+
+      </div>
+      <p>Ao planejar sua viagem pela plann.er você automaticamente concorda
+      com nossos <a href="#">termos de uso</a> e <a href="#">políticas de privacidade</a>.</p>
+    </div>
+  )
+}
